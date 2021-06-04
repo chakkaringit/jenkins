d0405b6024392a7a292135af606fb41537e7a214
chakkaringit <sluvpy.kh@gmail.com>
2021-06-04 14:21:13 +0700

    update

--- deploy-core.groovy
+++ deploy-core.groovy
@@ -2,10 +2,12 @@ pipeline {
   agent any
   stages {
     stage('Deploy Image to kubernetes') {
-       withKubeConfig([credentialsId: 'kubernate-cluster']) {
-        sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"'  
-        sh 'chmod u+x ./kubectl'  
-        sh './kubectl get pods'
+      steps{
+         withKubeConfig([credentialsId: 'kubernate-cluster']) {
+          sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"'  
+          sh 'chmod u+x ./kubectl'  
+          sh './kubectl get pods'
+        }
       }
     }
   }

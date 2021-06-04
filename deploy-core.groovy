3d6b61c1614026f276d8ecd964a9a25e5b2d14b8
chakkaringit <sluvpy.kh@gmail.com>
2021-06-04 14:37:32 +0700

    update

--- deploy-core.groovy
+++ deploy-core.groovy
@@ -12,14 +12,14 @@ pipeline {
          withKubeConfig([credentialsId: 'kubernate-cluster']) {
           sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"'  
           sh 'chmod u+x ./kubectl'  
-          sh './kubectl -n ${env.namespace} get pods'
+          sh './kubectl -n ns-volt-dev-v1 get pods'
         }
       }
     }
     stage('Deploy service to kubernetes') {
       steps{
          withKubeConfig([credentialsId: 'kubernate-cluster']) {
-          sh './kubectl -n ${env.namespace} apply -f values/deployment.yaml'
+          sh './kubectl -n ns-volt-dev-v1 apply -f values/deployment.yaml'
         }
       }
     }

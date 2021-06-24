7357cfc0042a9ebda1debe45842a9437c9874a49
chakkaringit <sluvpy.kh@gmail.com>
2021-06-24 11:00:25 +0700

    Update rollbackPlan.sh

--- scripts/rollbackPlan.sh
+++ scripts/rollbackPlan.sh
@@ -1,2 +1,23 @@
-status=`./kubectl -n ${1} get pods -lapp=email-alert-master-pod`
-echo "=== Check pod status ${status}"
+retryCount=0
+runningCount=0
+while true
+do
+  status=`./kubectl -n ${1} get pods -lapp=${2} --output=jsonpath='{.items[*].status.phase}'`
+  echo "Pod status ${status}"
+  
+  if [ "$status" = "Running" ] && [ ${runningCount} -ge 2 ]
+  then
+    break;
+  fi
+  
+  if [ "$status" != "Running" ] && [ ${runningCount} -ge 2 ]
+  then
+    echo "Need to rollback"
+    break;
+  fi
+  
+  sleep 5
+  runningCount=`expr ${runningCount} + 1`
+  retryCount=`expr ${retryCount} + 1`
+done
+

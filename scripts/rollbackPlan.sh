c23c393be743ad5669b7478f54d3a7a5865b9592
chakkaringit <sluvpy.kh@gmail.com>
2021-06-24 11:39:15 +0700

    Update rollbackPlan.sh

--- scripts/rollbackPlan.sh
+++ scripts/rollbackPlan.sh
@@ -1,5 +1,7 @@
 retryCount=0
 runningCount=0
+rollback=0
+statusMessage="with new version"
 while true
 do
   status=`./kubectl -n ${1} get pods -lapp=${2} --output=jsonpath='{.items[*].status.phase}'`
@@ -7,15 +9,35 @@ do
   
   if [ "$status" = "Running" ] && [ ${runningCount} -ge 2 ]
   then
+    if [ ${rollback} -eq 1 ]
+    then
+      statusMessage="with rollback version"
+    if
+    
+    echo "Service is Running ${statusMessage}"
     break;
   fi
   
-  if [ "$status" != "Running" ] && [ ${runningCount} -ge 2 ]
+  if [ "$status" != "Running" ] && [ ${retryCount} -ge 2 ] && [ ${rollback} -eq 0 ]
   then
-    echo "Need to rollback"
-    ./kubectl -n ${1} rollout history deployment ${3}
-    break;
+    echo "Start rollback process"
+    revision=`./kubectl -n ${1} rollout history deployment ${3} | awk '{print $1}' | head -3 | tail -1`
+    ./kubectl -n ${1} rollout history deployment ${3} 
+    
+    echo "Rollback to revision ${revision}"
+    ./kubectl -n ${1} rollout undo deployment/${3} --to-revision=${revision}
+    
+    rollback=`expr ${rollback} + 1`
+    runningCount=0
+    retryCount=0
+
   fi
+  
+  if [ "$status" != "Running" ] && [ ${retryCount} -ge 2 ] && [ ${rollback} -eq 1 ]
+  then
+    echo "Service is Not Running and Rollback not success!!!"
+    break;
+  if
   
   sleep 20
   runningCount=`expr ${runningCount} + 1`
